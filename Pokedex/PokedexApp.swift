0fbd37c3becaed5c7e41d76d4baf077daef91d05
Deonte Kilgore <deontecodes@gmail.com>
2021-06-05 15:13:36 -0400

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Deonte Kilgore on 6/5/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
